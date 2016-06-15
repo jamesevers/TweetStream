656076c7368c552940cd7ac33dd9b11481e44be3
Sean Scanlan <sean.scanlan@kettlenyc.com>
2016-06-15 18:19:57 -0400

    Updating with superagent

--- public/app.js
+++ public/app.js
@@ -1,13 +1,25 @@
 'use strict';
 
 var socket = io();
+var Ajax = require('superagent');
 socket.heartbeatTimeout = 20000;
 
 var App = (function() {
 
+	var ajax;
+
+	var initialize = function() {
+		Ajax
+		.get('http://api.imgflip.com/get_memes')
+		.end(function(err, res) {
+			console.log('what the fuck', err, res);
+		});
+	};
+
 	return {
 		init: function() {
 			console.log('we init');
+			initialize();
 			//socket.emit('tweet button clicked');
 		}
 	}
